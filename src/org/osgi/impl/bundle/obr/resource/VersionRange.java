7f4b84acf340a2fa2aac71148104077d91e91339
Peter Kriens <peter.kriens@aqute.biz>
2007-10-01 12:56:02 +0000

    Added compare functions

--- src/org/osgi/impl/bundle/obr/resource/VersionRange.java
+++ src/org/osgi/impl/bundle/obr/resource/VersionRange.java
@@ -22,15 +22,15 @@ import java.util.regex.*;
 
 import org.osgi.framework.*;
 
-public class VersionRange {
-	Version			high;
-	Version			low;
-	char			start	= '[';
-	char			end		= ']';
+public class VersionRange implements Comparable {
+	Version high;
+	Version low;
+	char start = '[';
+	char end = ']';
 
-	static String	V		= "[0-9]+(\\.[0-9]+(\\.[0-9]+(\\.[a-zA-Z0-9_-]+)?)?)?";
-	static Pattern	RANGE	= Pattern.compile("(\\(|\\[)(" + V + "),(" + V +
-									")(\\)|\\])");
+	static String V = "[0-9]+(\\.[0-9]+(\\.[0-9]+(\\.[a-zA-Z0-9_-]+)?)?)?";
+	static Pattern RANGE = Pattern.compile("(\\(|\\[)(" + V + "),(" + V
+			+ ")(\\)|\\])");
 
 	public VersionRange(String string) {
 		string = string.trim();
@@ -40,37 +40,80 @@ public class VersionRange {
 			low = new Version(m.group(2));
 			high = new Version(m.group(6));
 			end = m.group(10).charAt(0);
-			if ( low.compareTo(high) >=0 )
-				throw new IllegalArgumentException("Low Range is higher than High Range: " + low + "-" + high );
-			
-		}
-		else
+			if (low.compareTo(high) >= 0)
+				throw new IllegalArgumentException(
+						"Low Range is higher than High Range: " + low + "-"
+								+ high);
+
+		} else
 			high = low = new Version(string);
 	}
-
 
 	public boolean isRange() {
 		return high != low;
 	}
-	
+
 	public boolean includeLow() {
 		return start == '[';
 	}
-	
+
 	public boolean includeHigh() {
-		return start == ']';
+		return end == ']';
 	}
-	
+
 	public String toString() {
-		if ( high == low ) 
+		if (high == low)
 			return high.toString();
-		
+
 		StringBuffer sb = new StringBuffer();
 		sb.append(start);
 		sb.append(low);
 		sb.append(',');
 		sb.append(high);
-		sb.append(end);		
+		sb.append(end);
 		return sb.toString();
+	}
+
+	public boolean equals(Object other) {
+		if (other instanceof VersionRange) {
+			return compareTo(other)==0;
+		}
+		return false;
+	}
+
+	public int hashCode() {
+		return low.hashCode() * high.hashCode();
+	}
+
+	public int compareTo(Object other) {
+		VersionRange range = (VersionRange) other;
+		VersionRange a = this, b = range;
+		if (range.isRange()) {
+			a = range;
+			b = this;
+		} else {
+			if ( !isRange() )
+				return low.compareTo(range.high);
+		}
+		int l = a.low.compareTo(b.low);
+		boolean ll = false;
+		if (a.includeLow())
+			ll = l <= 0;
+		else
+			ll = l < 0;
+
+		if (!ll)
+			return -1;
+
+		int h = a.high.compareTo(b.high);
+		if (a.includeHigh())
+			ll = h >= 0;
+		else
+			ll = h > 0;
+
+		if (ll)
+			return 0;
+		else
+			return 1;
 	}
 }
